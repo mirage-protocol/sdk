58d9cd0d0346c79eada4cd264c67d6ed8b3dca24
moody <moodycoinz@protonmail.com>
2023-03-13 23:07:56 -0700

    fix usdc oracle

--- src/constants/priceFeeds.ts
+++ src/constants/priceFeeds.ts
@@ -1,11 +1,7 @@
 import { APTOS_NETWORK, PYTH_CLIENT } from '../constants/network'
 import { ValidMoveCoins } from './types'
 
-type CoinsWithPriceFeeds =
-  | ValidMoveCoins.APTOS
-  | ValidMoveCoins.MIRAGE_APTOS
-  | ValidMoveCoins.MIRAGE_ETHEREUM
-  | ValidMoveCoins.DEV_USDC
+type CoinsWithPriceFeeds = ValidMoveCoins.APTOS | ValidMoveCoins.MIRAGE_APTOS | ValidMoveCoins.MIRAGE_ETHEREUM
 
 export const PRICE_FEEDS: { readonly [coin in CoinsWithPriceFeeds]: string } = {
   [ValidMoveCoins.APTOS]:
@@ -20,10 +16,10 @@ export const PRICE_FEEDS: { readonly [coin in CoinsWithPriceFeeds]: string } = {
     APTOS_NETWORK === 'mainnet'
       ? '0xff61491a931112ddf1bd8147cd1b641375f79f5825126d665480874634fd0ace'
       : '0xca80ba6dc32e08d06f1aa886011eed1d77c77be9eb761cc10d72b7d0a2fd57a6',
-  [ValidMoveCoins.DEV_USDC]:
-    APTOS_NETWORK === 'mainnet'
-      ? '0xeaa020c61cc479712813461ce153894a96a6c00b21ed0cfc2798d1f9a9e9c94a'
-      : '0x41f3625971ca2ed2263e78573fe5ce23e13d2558ed3f2e47ab0f84fb9e7ae722',
+  // [ValidMoveCoins.DEV_USDC]:
+  //   APTOS_NETWORK === 'mainnet'
+  //     ? '0xeaa020c61cc479712813461ce153894a96a6c00b21ed0cfc2798d1f9a9e9c94a'
+  //     : '0x41f3625971ca2ed2263e78573fe5ce23e13d2558ed3f2e47ab0f84fb9e7ae722',
 }
 
 export function getPriceFeed(borrowCoin: ValidMoveCoins, collateralCoin: ValidMoveCoins): string {
