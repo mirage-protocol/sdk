14859b35b0c891e0cdb423da25b07db93805b795
moody <moodycoinz@protonmail.com>
2023-03-14 01:37:49 -0700

    rename type

--- src/constants/types.ts
+++ src/constants/types.ts
@@ -2,7 +2,7 @@ import { MIRAGE_FRAMEWORK_ACCOUNT } from '../constants/accounts'
 
 export const moduleAddress = MIRAGE_FRAMEWORK_ACCOUNT.address
 
-export enum ValidMoveCoins {
+export enum ValidMoveCoin {
   MIRA = 'MIRA',
   APTOS = 'APT',
   DEV_USDC = 'devUSDC',
@@ -13,26 +13,26 @@ export enum ValidMoveCoins {
   PANCAKE_APT_MUSD_LP = 'CAKE_APT_MUSD',
 }
 
-export type CoinMap = { readonly [coin in ValidMoveCoins]: string }
+export type CoinMap = { readonly [coin in ValidMoveCoin]: string }
 
 export const checkTicker = (ticker: string) => {
   switch (ticker) {
-    case ValidMoveCoins.MIRA:
-      return ValidMoveCoins.MIRA
-    case ValidMoveCoins.APTOS:
-      return ValidMoveCoins.APTOS
-    case ValidMoveCoins.DEV_USDC:
-      return ValidMoveCoins.DEV_USDC
-    case ValidMoveCoins.MIRAGE_APTOS:
-      return ValidMoveCoins.MIRAGE_APTOS
-    case ValidMoveCoins.MIRAGE_ETHEREUM:
-      return ValidMoveCoins.MIRAGE_ETHEREUM
-    case ValidMoveCoins.MIRAGE_USD:
-      return ValidMoveCoins.MIRAGE_USD
-    case ValidMoveCoins.ZL_USD:
-      return ValidMoveCoins.ZL_USD
-    case ValidMoveCoins.PANCAKE_APT_MUSD_LP:
-      return ValidMoveCoins.PANCAKE_APT_MUSD_LP
+    case ValidMoveCoin.MIRA:
+      return ValidMoveCoin.MIRA
+    case ValidMoveCoin.APTOS:
+      return ValidMoveCoin.APTOS
+    case ValidMoveCoin.DEV_USDC:
+      return ValidMoveCoin.DEV_USDC
+    case ValidMoveCoin.MIRAGE_APTOS:
+      return ValidMoveCoin.MIRAGE_APTOS
+    case ValidMoveCoin.MIRAGE_ETHEREUM:
+      return ValidMoveCoin.MIRAGE_ETHEREUM
+    case ValidMoveCoin.MIRAGE_USD:
+      return ValidMoveCoin.MIRAGE_USD
+    case ValidMoveCoin.ZL_USD:
+      return ValidMoveCoin.ZL_USD
+    case ValidMoveCoin.PANCAKE_APT_MUSD_LP:
+      return ValidMoveCoin.PANCAKE_APT_MUSD_LP
     default:
       throw new TypeError('Not a valid Coin')
   }
@@ -43,21 +43,21 @@ enum OtherTickers {
   BITCOIN = 'BTC',
 }
 
-export type ValidTickers = ValidMoveCoins | OtherTickers
+export type ValidTickers = ValidMoveCoin | OtherTickers
 
 export const TYPES: CoinMap = {
-  [ValidMoveCoins.APTOS]: '0x1::aptos_coin::AptosCoin',
-  [ValidMoveCoins.MIRAGE_ETHEREUM]: `${moduleAddress}::synth::METH`,
-  [ValidMoveCoins.MIRAGE_USD]: `${moduleAddress}::synth::MUSD`,
-  [ValidMoveCoins.MIRAGE_APTOS]: `${moduleAddress}::synth::MAPT`,
-  [ValidMoveCoins.MIRA]: `${moduleAddress}::mirage::Mirage`,
-  [ValidMoveCoins.DEV_USDC]:
+  [ValidMoveCoin.APTOS]: '0x1::aptos_coin::AptosCoin',
+  [ValidMoveCoin.MIRAGE_ETHEREUM]: `${moduleAddress}::synth::METH`,
+  [ValidMoveCoin.MIRAGE_USD]: `${moduleAddress}::synth::MUSD`,
+  [ValidMoveCoin.MIRAGE_APTOS]: `${moduleAddress}::synth::MAPT`,
+  [ValidMoveCoin.MIRA]: `${moduleAddress}::mirage::Mirage`,
+  [ValidMoveCoin.DEV_USDC]:
     '0x498d8926f16eb9ca90cab1b3a26aa6f97a080b3fcbe6e83ae150b7243a00fb68::devnet_coins::DevnetUSDC',
-  [ValidMoveCoins.ZL_USD]: '0xf22bede237a07e121b56d91a491eb7bcdfd1f5907926a9e58338f964a01b17fa::asset::USDC',
-  [ValidMoveCoins.PANCAKE_APT_MUSD_LP]: '#####TODO######',
+  [ValidMoveCoin.ZL_USD]: '0xf22bede237a07e121b56d91a491eb7bcdfd1f5907926a9e58338f964a01b17fa::asset::USDC',
+  [ValidMoveCoin.PANCAKE_APT_MUSD_LP]: '#####TODO######',
 }
 
-export const PRECISIONS: Record<ValidMoveCoins, number> = {
+export const PRECISIONS: Record<ValidMoveCoin, number> = {
   APT: 8,
   mUSD: 8,
   mAPT: 8,
