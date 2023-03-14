270b028172b152646db7d4de7cb452fadf3bfc07
moody <moodycoinz@protonmail.com>
2023-03-14 13:43:22 -0700

    add: coin class variables

--- src/Coin.ts
+++ src/Coin.ts
@@ -4,12 +4,15 @@ import { PRECISIONS, Resource, TYPES, ValidMoveCoin } from './constants/types'
 
 export default class Coin {
   balance: BigNumber
+  coinType: ValidMoveCoin
+  precision: BigNumber
 
-  constructor(resources: Resource[], coin: ValidMoveCoin) {
-    const coinStore = resources.find((resource) => resource.type == `0x1::coin::CoinStore<${TYPES[coin]}>`)
+  constructor(resources: Resource[], coinType: ValidMoveCoin) {
+    const coinStore = resources.find((resource) => resource.type == `0x1::coin::CoinStore<${TYPES[coinType]}>`)
+    const precision = BigNumber(10).pow(PRECISIONS[coinType])
 
-    this.balance = !!coinStore
-      ? new BigNumber((coinStore.data as any).coin.value).div(BigNumber(10).pow(PRECISIONS[coin]))
-      : new BigNumber(0)
+    this.precision = precision
+    this.coinType = coinType
+    this.balance = !!coinStore ? new BigNumber((coinStore.data as any).coin.value).div(precision) : new BigNumber(0)
   }
 }
