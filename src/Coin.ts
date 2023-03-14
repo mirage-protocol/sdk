ff30fa08d3d9d4e44b11a605b40c470ed4e00e0b
moody <moodycoinz@protonmail.com>
2023-03-14 16:33:06 -0700

    refactor: allow making an empty coin

--- src/Coin.ts
+++ src/Coin.ts
@@ -7,12 +7,14 @@ export default class Coin {
   coinType: ValidMoveCoin
   precision: BigNumber
 
-  constructor(resources: Resource[], coinType: ValidMoveCoin) {
-    const coinStore = resources.find((resource) => resource.type == `0x1::coin::CoinStore<${TYPES[coinType]}>`)
+  constructor(resources: Resource[] | null | undefined, coinType: ValidMoveCoin) {
     const precision = BigNumber(10).pow(PRECISIONS[coinType])
-
     this.precision = precision
     this.coinType = coinType
-    this.balance = !!coinStore ? new BigNumber((coinStore.data as any).coin.value).div(precision) : new BigNumber(0)
+    if (!resources) this.balance = new BigNumber(0)
+    else {
+      const coinStore = resources.find((resource) => resource.type == `0x1::coin::CoinStore<${TYPES[coinType]}>`)
+      this.balance = !!coinStore ? new BigNumber((coinStore.data as any).coin.value).div(precision) : new BigNumber(0)
+    }
   }
 }
