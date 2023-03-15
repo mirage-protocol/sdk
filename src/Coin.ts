a246c63ac5c4953627652c1624092987de319170
moody <moodycoinz@protonmail.com>
2023-03-15 02:35:05 -0700

    add: ui balance fnc

--- src/Coin.ts
+++ src/Coin.ts
@@ -4,6 +4,9 @@ import { ZERO } from './constants'
 import { AccountResource } from './constants/accounts'
 import { coinInfo, MoveCoin } from './constants/coinList'
 
+export const toUi = (bn: BigNumber, coin: MoveCoin): number =>
+  bn.div(BigNumber(10).pow(coinInfo(coin).decimals)).toNumber()
+
 export default class Coin {
   public readonly name: string
   public readonly symbol: string
@@ -31,5 +34,9 @@ export default class Coin {
       const coinStore = resources.find((resource) => resource.type == `0x1::coin::CoinStore<${type}>`)
       this.balance = !!coinStore ? new BigNumber((coinStore.data as any).coin.value).div(precision) : ZERO
     }
+  }
+
+  public getUiBalance(): number {
+    return toUi(this.balance, this.coinType)
   }
 }
