a95f26390b9a64931bf23e2d3530a40005135a61
moody <moodycoinz@protonmail.com>
2023-03-17 23:57:25 -0700

    fix: enum to string type

--- src/constants/coinList.ts
+++ src/constants/coinList.ts
@@ -1,28 +1,28 @@
 import { HexString } from 'aptos'
 import BigNumber from 'bignumber.js'
 
-import { DEV_USDC_ADDRESS, LZ_ADDRESS, MIRAGE_ADDRESS, PANCAKE_ADDRESS } from './accounts'
+import { getModuleAddress, mirageAddress } from './accounts'
 
 /**
  * All Coins relevant to the mirage-protocol ecosystem
  */
 export enum MoveCoin {
-  MIRA,
-  APT,
-  mAPT,
-  mETH,
-  mUSD,
-  zUSDC,
-  devUSDC,
-  PANCAKE_APT_MUSD_LP,
+  MIRA = 'MIRA', // Mirage coin
+  APT = 'APT', // Aptos coin
+  mAPT = 'mAPT', // mirage-Aptos
+  mETH = 'mETH', // mirage-Ethereum
+  mUSD = 'mUSD', // mirage-Usd
+  zUSDC = 'zUSDC', // Layer-zero USDC
+  devUSDC = 'devUSDC', // devnet USDC
+  APT_MUSD_LP = 'APT_MUSD_LP', // APT/MUSD LP
 }
 
 /**
  * Other off-chain assets
  */
 export enum OtherAsset {
-  BTC,
-  ETH,
+  BTC = 'BTC',
+  ETH = 'ETH',
 }
 
 /**
@@ -47,14 +47,18 @@ export type CoinInfo = {
  * @param symbol string symbol of coin
  * @returns the MoveCoin or undefined if not valid
  */
-export const checkTicker = (symbol: string): MoveCoin | undefined => MoveCoin[symbol]
+export const checkSymbol = (symbol: string): MoveCoin | undefined => {
+  return MoveCoin[symbol]
+}
 
 /**
  * Get info about a specific coin
  * @param coin the MoveCoin to get info for
  * @returns the CoinInfo for the specific coin
  */
-export const coinInfo = (coin: MoveCoin): CoinInfo => mirageCoinList[coin]
+export const coinInfo = (coin: MoveCoin | string): CoinInfo => {
+  return mirageCoinList[coin as keyof typeof MoveCoin]
+}
 
 /**
  * Get the balance of a coin in a Ui friendly format
@@ -62,8 +66,9 @@ export const coinInfo = (coin: MoveCoin): CoinInfo => mirageCoinList[coin]
  * @param coin the coin
  * @returns a human-readable balance value
  */
-export const balanceToUi = (balance: BigNumber, coin: MoveCoin): number =>
-  balance.div(BigNumber(10).pow(coinInfo(coin).decimals)).toNumber()
+export const balanceToUi = (balance: BigNumber, coin: MoveCoin | string): number => {
+  return balance.div(BigNumber(10).pow(coinInfo(coin).decimals)).toNumber()
+}
 
 // A list of all coins and their info in the Mirage ecosystem
 const mirageCoinList: { readonly [coin in MoveCoin]: CoinInfo } = {
@@ -79,52 +84,52 @@ const mirageCoinList: { readonly [coin in MoveCoin]: CoinInfo } = {
     name: 'Mirage Coin',
     symbol: 'MIRA',
     decimals: 8,
-    address: MIRAGE_ADDRESS,
-    type: `${MIRAGE_ADDRESS}::mirage::Mirage`,
+    address: mirageAddress(),
+    type: `${mirageAddress()}::mirage::Mirage`,
   },
   [MoveCoin.mUSD]: {
     name: 'Mirage USD',
     symbol: 'mUSD',
     decimals: 8,
-    address: MIRAGE_ADDRESS,
-    type: `${MIRAGE_ADDRESS}::synth::MUSD`,
+    address: mirageAddress(),
+    type: `${mirageAddress()}::synth::MUSD`,
   },
   [MoveCoin.mAPT]: {
     name: 'Mirage Aptos',
     symbol: 'mAPT',
     decimals: 8,
-    address: MIRAGE_ADDRESS,
-    type: `${MIRAGE_ADDRESS}::synth::MAPT`,
+    address: mirageAddress(),
+    type: `${mirageAddress()}::synth::MAPT`,
   },
   [MoveCoin.mETH]: {
     name: 'Mirage Ethereum',
     symbol: 'mETH',
     decimals: 8,
-    address: MIRAGE_ADDRESS,
-    type: `${MIRAGE_ADDRESS}::synth::METH`,
+    address: mirageAddress(),
+    type: `${mirageAddress()}::synth::METH`,
   },
   [MoveCoin.zUSDC]: {
     name: 'Layer-Zero USDC',
     symbol: 'zUSDC',
     decimals: 6,
-    address: LZ_ADDRESS,
-    type: `${LZ_ADDRESS}::asset::USDC`,
+    address: getModuleAddress('layer_zero'),
+    type: `${getModuleAddress('layer_zero')}::asset::USDC`,
     logoUrl: 'https://raw.githubusercontent.com/hippospace/aptos-coin-list/main/icons/USDC.svg',
   },
   [MoveCoin.devUSDC]: {
     name: 'Testnet USDC',
     symbol: 'devUSDC',
     decimals: 8,
-    address: DEV_USDC_ADDRESS,
-    type: `${DEV_USDC_ADDRESS}::devnet_coins::DevnetUSDC`,
+    address: getModuleAddress('dev_usdc'),
+    type: `${getModuleAddress('dev_usdc')}::devnet_coins::DevnetUSDC`,
     logoUrl: 'https://raw.githubusercontent.com/hippospace/aptos-coin-list/main/icons/USDC.svg',
   },
   // TODO
-  [MoveCoin.PANCAKE_APT_MUSD_LP]: {
+  [MoveCoin.APT_MUSD_LP]: {
     name: 'MUSD LP Coin',
     symbol: 'musd-lp',
     decimals: 8,
-    address: PANCAKE_ADDRESS,
+    address: getModuleAddress('pancake'),
     type: 'TODO',
   },
 }
