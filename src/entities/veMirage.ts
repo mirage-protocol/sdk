0ae54ec1d9a1d83adf2e185dbda3f62772217ae5
MoonShiesty <moonshiesty@protonmail.com>
2023-04-25 08:49:58 +0000

    add veMirage

---
+++ src/entities/veMirage.ts
@@ -0,0 +1,46 @@
+import BigNumber from 'bignumber.js'
+
+import { ZERO } from '../constants'
+import { Rebase } from './rebase'
+import { AccountResource, mirageAddress } from '../constants/accounts'
+
+/**
+ * Represent VeMirage struct.
+ * Stores info about the global vote-escrow lock data.
+ */
+export class VeMirage
+{
+    /**
+    * The rebase representing mirage locked and accrued rewards.
+    */
+    public readonly lock: Rebase
+    /**
+    * The total locked mirage tokens.
+    */
+    public readonly totalLocked: BigNumber
+    /**
+    * The total veMira supplied.
+    */
+    public readonly totalSupply: BigNumber
+
+    /**
+     * Construct an instance of UserInfo
+     * @param moduleResources resources for the veMirage account (MIRAGE_ACCOUNT)
+     */
+    constructor(
+        moduleResources: AccountResource[]
+    ) {
+        let moveType = `${mirageAddress()}::ve_mirage::VeMirage`
+        console.debug(`attempting to get data for type: ${moveType}`)
+
+        const veMirage = moduleResources.find((resource) => resource.type === this.vaultType)
+
+        console.debug(`found data: ${veMirage}`)
+        this.lock = !!veMirage
+          ? new Rebase(BigNumber((veMirage.data as any).lock.elastic), BigNumber((veMirage.data as any).lock.base))
+          : new Rebase(ZERO, ZERO)
+
+        this.totalLocked = !!veMirage ? BigNumber((veMirage.data as any).total_locked) : ZERO
+        this.totalSupply = !!veMirage ? BigNumber((veMirage.data as any).total_supply) : ZERO
+    }
+}
