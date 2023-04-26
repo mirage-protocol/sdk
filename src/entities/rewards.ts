bcad45b8553bce722cec1298c7491cb2fc1a2ac3
MoonShiesty <moonshiesty@protonmail.com>
2023-04-26 16:45:11 +0000

    add veMirage payloads and entities

--- src/entities/rewards.ts
+++ src/entities/rewards.ts
@@ -7,33 +7,30 @@ import { AccountResource, mirageAddress } from '../constants/accounts'
  * Represent Rewards struct.
  * Stores info about the global vote-escrow rewards data.
  */
-export class Rewards
-{
-    /**
-    * The base reward rate.
-    */
-    public readonly baseRewardRate: BigNumber
-    /**
-    * The last time rewards were updated.
-    */
-    public readonly lastUpdated: BigNumber
+export class Rewards {
+  /**
+   * The base reward rate.
+   */
+  public readonly baseRewardRate: BigNumber
+  /**
+   * The last time rewards were updated.
+   */
+  public readonly lastUpdated: BigNumber
 
-    /**
-     * Construct an instance of UserInfo
-     * @param moduleResources resources for the veMirage account (MIRAGE_ACCOUNT)
-     */
-    constructor(
-        moduleResources: AccountResource[]
-    ) {
-        let moveType = `${mirageAddress()}::ve_mirage::Rewards`
+  /**
+   * Construct an instance of UserInfo
+   * @param moduleResources resources for the veMirage account (MIRAGE_ACCOUNT)
+   */
+  constructor(moduleResources: AccountResource[]) {
+    const moveType = `${mirageAddress()}::ve_mirage::Rewards`
 
-        console.debug(`attempting to get data for type: ${moveType}`)
+    console.debug(`attempting to get data for type: ${moveType}`)
 
-        const veMirage = moduleResources.find((resource) => resource.type === this.vaultType)
+    const veMirage = moduleResources.find((resource) => resource.type === moveType)
 
-        console.debug(`found data: ${veMirage}`)
+    console.debug(`found data: ${veMirage}`)
 
-        this.baseRewardRate = !!veMirage ? BigNumber((veMirage.data as any).baseRewardRate) : ZERO
-        this.lastUpdated = !!veMirage ? BigNumber((veMirage.data as any).lastUpdated) : ZERO
-    }
+    this.baseRewardRate = !!veMirage ? BigNumber((veMirage.data as any).baseRewardRate) : ZERO
+    this.lastUpdated = !!veMirage ? BigNumber((veMirage.data as any).lastUpdated) : ZERO
+  }
 }
--- src/entities/voteLock.ts
+++ src/entities/voteLock.ts
@@ -1,5 +1,6 @@
 import BigNumber from 'bignumber.js'
 
+import { VeMirage } from './veMirage'
 import { ZERO } from '../constants'
 import { AccountResource, mirageAddress } from '../constants/accounts'
 
@@ -7,49 +8,56 @@ import { AccountResource, mirageAddress } from '../constants/accounts'
  * Represent VoteLock struct.
  * Stores info about the global vote-escrow lock data.
  */
-export class VoteLock
-{
-    /**
-    * The amount of MIRA the user has locked.
-    */
-    public readonly locked: BigNumber
-    /**
-    * The amount time multiplier.
-    */
-    public readonly scaledAmount: BigNumber
-    /**
-    * The time when the user can withdraw entire all locked MIRA.
-    */
-    public readonly unlockTime: BigNumber
-    /**
-    * The time when the user initially locked MIRA.
-    */
-    public readonly lockTime: BigNumber
-    /**
-    * The users reward multiplier for lock time.
-    */
-    public readonly multiplier: BigNumber
+export class VoteLock {
+  /**
+   * The amount of MIRA the user has locked.
+   */
+  public readonly locked: BigNumber
+  /**
+   * The amount time multiplier.
+   */
+  public readonly scaledAmount: BigNumber
+  /**
+   * The time when the user can withdraw entire all locked MIRA.
+   */
+  public readonly unlockTime: BigNumber
+  /**
+   * The time when the user initially locked MIRA.
+   */
+  public readonly lockTime: BigNumber
+  /**
+   * The users reward multiplier for lock time.
+   */
+  public readonly multiplier: BigNumber
 
-    /**
-     * Construct an instance of UserInfo
-     * @param moduleResources resources for the veMirage account (MIRAGE_ACCOUNT)
-     */
-    constructor(
-        userResources: AccountResource[],
-        moduleResources: AccountResource[]
-    ) {
-        let voteLockType = `${mirageAddress()}::ve_mirage::VeMirage`
+  /**
+   * An instance of VeMirage for this VoteLock
+   */
+  public readonly veMirage: VeMirage
 
-        console.debug(`attempting to get data for type: ${voteLockType}`)
+  /**
+   * Construct an instance of UserInfo
+   * @param moduleResources resources for the veMirage account (MIRAGE_ACCOUNT)
+   */
+  constructor(userResources: AccountResource[], moduleResources: AccountResource[]) {
+    const voteLockType = `${mirageAddress()}::ve_mirage::VeMirage`
 
-        const voteLock = userResources.find((resource) => resource.type === voteLockType)
+    console.debug(`attempting to get data for type: ${voteLockType}`)
 
-        console.debug(`found data: ${voteLock}`)
+    const voteLock = userResources.find((resource) => resource.type === voteLockType)
 
-        this.locked = !!voteLock ? BigNumber((voteLock.data as any).locked) : ZERO
-        this.scaledAmount = !!voteLock ? BigNumber((voteLock.data as any).scaledAmount) : ZERO
-        this.unlockTime = !!voteLock ? BigNumber((voteLock.data as any).unlockTime) : ZERO
-        this.lockTime = !!voteLock ? BigNumber((voteLock.data as any).lockTime) : ZERO
-        this.multiplier = !!voteLock ? BigNumber((voteLock.data as any).multiplier) : ZERO
-    }
+    console.debug(`found data: ${voteLock}`)
+
+    this.veMirage = new VeMirage(moduleResources);
+
+    this.locked =
+      !!voteLock && !!this.veMirage
+        ? this.veMirage.lock.toElastic(new BigNumber((voteLock.data as any).locked), true)
+        : ZERO
+
+    this.scaledAmount = !!voteLock ? BigNumber((voteLock.data as any).scaledAmount) : ZERO
+    this.unlockTime = !!voteLock ? BigNumber((voteLock.data as any).unlockTime) : ZERO
+    this.lockTime = !!voteLock ? BigNumber((voteLock.data as any).lockTime) : ZERO
+    this.multiplier = !!voteLock ? BigNumber((voteLock.data as any).multiplier) : ZERO
+  }
 }
