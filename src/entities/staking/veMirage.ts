54a9f8e24ff328e1383d5f89e19510ddfffeb808
moody <moodycoinz@protonmail.com>
2023-05-12 22:01:24 -0700

    add: baseApr

--- src/entities/staking/veMirage.ts
+++ src/entities/staking/veMirage.ts
@@ -1,18 +1,13 @@
 import BigNumber from 'bignumber.js'
 
-import { ZERO } from '../../constants'
+import { balanceToUi, MoveCoin, ZERO } from '../../constants'
 import { AccountResource, mirageAddress } from '../../constants/accounts'
-import { Rebase } from '../rebase'
 
 /**
  * Represent VeMirage struct.
  * Stores info about the global vote-escrow lock data.
  */
 export class VeMirage {
-  /**
-   * The rebase representing mirage locked and accrued rewards.
-   */
-  public readonly lock: Rebase
   /**
    * The total locked mirage tokens.
    */
@@ -35,25 +30,56 @@ export class VeMirage {
   public readonly maxLockTime: number = 94608000
 
   /**
+   * The current percentage apr
+   */
+  public readonly baseApr: number
+
+  /**
    * Construct an instance of UserInfo
    * @param moduleResources resources for the veMirage account (MIRAGE_ACCOUNT)
    */
   constructor(moduleResources: AccountResource[]) {
     const veMirageType = `${mirageAddress()}::ve_mirage::VeMirage`
+    const rewardsType = `${mirageAddress()}::ve_mirage::Rewards`
 
     console.debug(`attempting to get data for type: ${veMirageType}`)
 
     const veMirage = moduleResources.find((resource) => resource.type === veMirageType)
+    const rewards = moduleResources.find((resource) => resource.type === rewardsType)
 
     console.debug(`found data: ${veMirage}`)
-
-    this.lock = !!veMirage
-      ? new Rebase(BigNumber((veMirage.data as any).lock.elastic), BigNumber((veMirage.data as any).lock.base))
-      : new Rebase(ZERO, ZERO)
 
     this.totalLocked = !!veMirage ? BigNumber((veMirage.data as any).locked.value) : ZERO
     this.totalSupply = !!veMirage ? BigNumber((veMirage.data as any).total_supply) : ZERO
 
     this.maxUnlockPenalty = !!veMirage ? ((veMirage.data as any).max_unlock_penalty * 100) / 10000 : 0
+
+    const rewardRate = !!rewards ? BigNumber((rewards.data as any).base_reward_rate) : ZERO
+
+    if (rewardRate.gt(0)) {
+      const rewardsPerYear = rewardRate.times(31536000)
+
+      const apr = rewardsPerYear.div(this.totalLocked).toNumber()
+
+      this.baseApr = apr * 100
+    } else {
+      this.baseApr = 0
+    }
+  }
+
+  /**
+   * Get the total locked Mirage supply (no precision)
+   * @returns The ve supply divided by the precision
+   */
+  public getUiTotalLocked(): number {
+    return balanceToUi(this.totalLocked, MoveCoin.MIRA)
+  }
+
+  /**
+   * Get the total veMira supply (no precision)
+   * @returns The ve supply divided by the precision
+   */
+  public getUiVeSupply(): number {
+    return balanceToUi(this.totalSupply, MoveCoin.MIRA)
   }
 }
