45f217932c6dedf0be86cd4f04169651c7f07223
MoonShiesty <moonshiesty@protonmail.com>
2023-05-27 00:47:34 +0000

    cleanup transactions

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -10,6 +10,7 @@ export const SECONDS_PER_YEAR = 31622400
 export const FUNDING_PRECISION = 1000000
 export const RATE_PRECISION = 100000000
 export const U64_MAX = '18446744073709551616'
+export const MIN_TRIGGER_PAYMENT = 1000000
 
 export * from './accounts'
 export * from './coinList'
