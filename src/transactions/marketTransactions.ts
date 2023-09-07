ee002e4b49df98872fa790b89cbbe4b44369dffc
moody <moodycoinz@protonmail.com>
2023-09-07 10:50:30 -0700

    refactor: script modules

--- src/transactions/marketTransactions.ts
+++ src/transactions/marketTransactions.ts
@@ -1,4 +1,3 @@
-import { TxnBuilderTypes } from 'aptos'
 import BigNumber from 'bignumber.js'
 
 import {
@@ -12,17 +11,8 @@ import {
   Network,
   Perpetual,
 } from '../constants'
-import { getScriptBytecode } from '../constants/scripts'
 import { PositionSide } from '../entities'
-import {
-  EntryFunctionPayload,
-  getBCSCoinAmountArgument,
-  getBCSDecimal8Argument,
-  getDecimal8Argument,
-  MoveType,
-  Payload,
-  PayloadResult,
-} from './'
+import { EntryFunctionPayload, getDecimal8Argument, MoveType, Payload } from './'
 import { getCoinAmountArgument } from './'
 
 const type = 'entry_function_payload'
@@ -49,57 +39,56 @@ export const openPosition = async (
   stopLossPrice: number,
   triggerPaymentAmount: number,
   network: Network
-): Promise<PayloadResult> => {
+): Promise<Payload> => {
   const marginFeed = getPriceFeed(marginCoin, network)
   const perpetualFeed = getPriceFeed(perpetual, network)
 
   const marginVaas = marginFeed ? await getPriceFeedUpdateData(marginFeed, getNetwork(network)) : []
   const perpetualVaas = perpetualFeed ? await getPriceFeedUpdateData(perpetualFeed, getNetwork(network)) : []
 
-  if (isInitialized) {
-    return {
-      natural: {
-        function: `${mirageAddress()}::market::open_position`,
-        type,
-        arguments: [
-          perpetualVaas,
-          marginVaas,
-          getDecimal8Argument(marginAmount), // always 8 decimals
-          getDecimal8Argument(positionSize),
-          side == PositionSide.LONG,
-          getDecimal8Argument(desired_price),
-          getDecimal8Argument(maxPriceSlippage),
-          getDecimal8Argument(takeProfitPrice),
-          getDecimal8Argument(stopLossPrice),
-          getCoinAmountArgument(MoveCoin.APT, triggerPaymentAmount),
-        ],
-        type_arguments: getMarketTypeArguments(marginCoin, perpetual),
-      },
-    }
-  }
   return {
-    bcs: new TxnBuilderTypes.TransactionPayloadScript(
-      new TxnBuilderTypes.Script(
-        getScriptBytecode('register_and_open_position'),
-        [
-          new TxnBuilderTypes.TypeTagStruct(TxnBuilderTypes.StructTag.fromString(assetInfo(marginCoin).type)),
-          new TxnBuilderTypes.TypeTagStruct(TxnBuilderTypes.StructTag.fromString(assetInfo(perpetual).type)),
-        ],
-        [
-          new TxnBuilderTypes.TransactionArgumentU8Vector(Uint8Array.from(perpetualVaas)),
-          new TxnBuilderTypes.TransactionArgumentU8Vector(Uint8Array.from(marginVaas)),
-          new TxnBuilderTypes.TransactionArgumentU64(getBCSDecimal8Argument(marginAmount)),
-          new TxnBuilderTypes.TransactionArgumentU64(getBCSDecimal8Argument(positionSize)),
-          new TxnBuilderTypes.TransactionArgumentBool(side == PositionSide.LONG),
-          new TxnBuilderTypes.TransactionArgumentU64(getBCSDecimal8Argument(desired_price)),
-          new TxnBuilderTypes.TransactionArgumentU64(getBCSDecimal8Argument(maxPriceSlippage)),
-          new TxnBuilderTypes.TransactionArgumentU64(getBCSDecimal8Argument(takeProfitPrice)),
-          new TxnBuilderTypes.TransactionArgumentU64(getBCSDecimal8Argument(stopLossPrice)),
-          new TxnBuilderTypes.TransactionArgumentU64(getBCSCoinAmountArgument(MoveCoin.APT, triggerPaymentAmount)),
-        ]
-      )
-    ),
+    function: isInitialized
+      ? `${mirageAddress()}::market::open_position`
+      : `${mirageAddress()}::market_scripts::register_and_open_position`,
+    type,
+    arguments: [
+      perpetualVaas,
+      marginVaas,
+      getDecimal8Argument(marginAmount), // always 8 decimals
+      getDecimal8Argument(positionSize),
+      side == PositionSide.LONG,
+      getDecimal8Argument(desired_price),
+      getDecimal8Argument(maxPriceSlippage),
+      getDecimal8Argument(takeProfitPrice),
+      getDecimal8Argument(stopLossPrice),
+      getCoinAmountArgument(MoveCoin.APT, triggerPaymentAmount),
+    ],
+    type_arguments: getMarketTypeArguments(marginCoin, perpetual),
   }
+
+  // return {
+  //   bcs: new TxnBuilderTypes.TransactionPayloadScript(
+  //     new TxnBuilderTypes.Script(
+  //       getScriptBytecode('register_and_open_position'),
+  //       [
+  //         new TxnBuilderTypes.TypeTagStruct(TxnBuilderTypes.StructTag.fromString(assetInfo(marginCoin).type)),
+  //         new TxnBuilderTypes.TypeTagStruct(TxnBuilderTypes.StructTag.fromString(assetInfo(perpetual).type)),
+  //       ],
+  //       [
+  //         new TxnBuilderTypes.TransactionArgumentU8Vector(Uint8Array.from(perpetualVaas)),
+  //         new TxnBuilderTypes.TransactionArgumentU8Vector(Uint8Array.from(marginVaas)),
+  //         new TxnBuilderTypes.TransactionArgumentU64(getBCSDecimal8Argument(marginAmount)),
+  //         new TxnBuilderTypes.TransactionArgumentU64(getBCSDecimal8Argument(positionSize)),
+  //         new TxnBuilderTypes.TransactionArgumentBool(side == PositionSide.LONG),
+  //         new TxnBuilderTypes.TransactionArgumentU64(getBCSDecimal8Argument(desired_price)),
+  //         new TxnBuilderTypes.TransactionArgumentU64(getBCSDecimal8Argument(maxPriceSlippage)),
+  //         new TxnBuilderTypes.TransactionArgumentU64(getBCSDecimal8Argument(takeProfitPrice)),
+  //         new TxnBuilderTypes.TransactionArgumentU64(getBCSDecimal8Argument(stopLossPrice)),
+  //         new TxnBuilderTypes.TransactionArgumentU64(getBCSCoinAmountArgument(MoveCoin.APT, triggerPaymentAmount)),
+  //       ]
+  //     )
+  //   ),
+  // }
 }
 
 /**
@@ -141,55 +130,75 @@ export const placeLimitOrder = async (
   triggerPaymentAmount: number,
   expiration: BigNumber, // in seconds
   network: Network
-): Promise<PayloadResult> => {
+): Promise<Payload> => {
   const perpetualFeed = getPriceFeed(perpetualAsset, network)
   const perpetualVaas = perpetualFeed ? await getPriceFeedUpdateData(perpetualFeed, getNetwork(network)) : []
 
-  if (isInitialized) {
-    return {
-      natural: {
-        type,
-        function: `${mirageAddress()}::market::place_limit_order`,
-        arguments: [
-          perpetualVaas,
-          getDecimal8Argument(marginAmount), // always 8 decimals
-          getDecimal8Argument(positionSize),
-          side == PositionSide.LONG,
-          getDecimal8Argument(triggerPrice),
-          getDecimal8Argument(maxPriceSlippage),
-          isIncrease,
-          triggersAbove,
-          getDecimal8Argument(triggerPaymentAmount),
-          expiration.toFixed(0),
-        ],
-        type_arguments: getMarketTypeArguments(marginCoin, perpetualAsset),
-      },
-    }
+  return {
+    type,
+    function: isInitialized
+      ? `${mirageAddress()}::market::place_limit_order`
+      : `${mirageAddress()}::market_scripts::register_and_place_limit`,
+    arguments: [
+      perpetualVaas,
+      getDecimal8Argument(marginAmount), // always 8 decimals
+      getDecimal8Argument(positionSize),
+      side == PositionSide.LONG,
+      getDecimal8Argument(triggerPrice),
+      getDecimal8Argument(maxPriceSlippage),
+      isIncrease,
+      triggersAbove,
+      getDecimal8Argument(triggerPaymentAmount),
+      expiration.toFixed(0),
+    ],
+    type_arguments: getMarketTypeArguments(marginCoin, perpetualAsset),
   }
 
-  return {
-    bcs: new TxnBuilderTypes.TransactionPayloadScript(
-      new TxnBuilderTypes.Script(
-        getScriptBytecode('register_and_place_limit'),
-        [
-          new TxnBuilderTypes.TypeTagStruct(TxnBuilderTypes.StructTag.fromString(assetInfo(marginCoin).type)),
-          new TxnBuilderTypes.TypeTagStruct(TxnBuilderTypes.StructTag.fromString(assetInfo(perpetualAsset).type)),
-        ],
-        [
-          new TxnBuilderTypes.TransactionArgumentU8Vector(Uint8Array.from(perpetualVaas)),
-          new TxnBuilderTypes.TransactionArgumentU64(getBCSDecimal8Argument(marginAmount)),
-          new TxnBuilderTypes.TransactionArgumentU64(getBCSDecimal8Argument(positionSize)),
-          new TxnBuilderTypes.TransactionArgumentBool(side == PositionSide.LONG),
-          new TxnBuilderTypes.TransactionArgumentU64(getBCSDecimal8Argument(triggerPrice)),
-          new TxnBuilderTypes.TransactionArgumentU64(getBCSDecimal8Argument(maxPriceSlippage)),
-          new TxnBuilderTypes.TransactionArgumentBool(isIncrease),
-          new TxnBuilderTypes.TransactionArgumentBool(triggersAbove),
-          new TxnBuilderTypes.TransactionArgumentU64(getBCSDecimal8Argument(triggerPaymentAmount)),
-          new TxnBuilderTypes.TransactionArgumentU64(BigInt(expiration.integerValue().toFixed(0))),
-        ]
-      )
-    ),
-  }
+  // if (isInitialized) {
+  //   return {
+  //     natural: {
+  //       type,
+  //       function: `${mirageAddress()}::market::place_limit_order`,
+  //       arguments: [
+  //         perpetualVaas,
+  //         getDecimal8Argument(marginAmount), // always 8 decimals
+  //         getDecimal8Argument(positionSize),
+  //         side == PositionSide.LONG,
+  //         getDecimal8Argument(triggerPrice),
+  //         getDecimal8Argument(maxPriceSlippage),
+  //         isIncrease,
+  //         triggersAbove,
+  //         getDecimal8Argument(triggerPaymentAmount),
+  //         expiration.toFixed(0),
+  //       ],
+  //       type_arguments: getMarketTypeArguments(marginCoin, perpetualAsset),
+  //     },
+  //   }
+  // }
+
+  // return {
+  //   bcs: new TxnBuilderTypes.TransactionPayloadScript(
+  //     new TxnBuilderTypes.Script(
+  //       getScriptBytecode('register_and_place_limit'),
+  //       [
+  //         new TxnBuilderTypes.TypeTagStruct(TxnBuilderTypes.StructTag.fromString(assetInfo(marginCoin).type)),
+  //         new TxnBuilderTypes.TypeTagStruct(TxnBuilderTypes.StructTag.fromString(assetInfo(perpetualAsset).type)),
+  //       ],
+  //       [
+  //         new TxnBuilderTypes.TransactionArgumentU8Vector(Uint8Array.from(perpetualVaas)),
+  //         new TxnBuilderTypes.TransactionArgumentU64(getBCSDecimal8Argument(marginAmount)),
+  //         new TxnBuilderTypes.TransactionArgumentU64(getBCSDecimal8Argument(positionSize)),
+  //         new TxnBuilderTypes.TransactionArgumentBool(side == PositionSide.LONG),
+  //         new TxnBuilderTypes.TransactionArgumentU64(getBCSDecimal8Argument(triggerPrice)),
+  //         new TxnBuilderTypes.TransactionArgumentU64(getBCSDecimal8Argument(maxPriceSlippage)),
+  //         new TxnBuilderTypes.TransactionArgumentBool(isIncrease),
+  //         new TxnBuilderTypes.TransactionArgumentBool(triggersAbove),
+  //         new TxnBuilderTypes.TransactionArgumentU64(getBCSDecimal8Argument(triggerPaymentAmount)),
+  //         new TxnBuilderTypes.TransactionArgumentU64(BigInt(expiration.integerValue().toFixed(0))),
+  //       ]
+  //     )
+  //   ),
+  // }
 }
 
 /**
