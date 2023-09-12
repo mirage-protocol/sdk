dc88dd6f5ec12775b0d08c861ebc49c0220cd2d8
MoonShiesty <moonshiesty@protonmail.com>
2023-09-12 15:40:51 -0500

    add testnet distributor api

---
+++ src/entities/distributor/userClaimDetails.ts
@@ -0,0 +1,13 @@
+import BigNumber from 'bignumber.js'
+
+import { AccountResource, mirageAddress, ZERO } from '../../constants'
+
+export class UserClaimDetails {
+  public readonly lastClaimTimestamp: BigNumber
+
+  constructor(userResource: AccountResource[]) {
+    const type = `${mirageAddress()}::distributor::UserClaimDetailst`
+    const claim = userResource.find((resource) => resource.type === type)
+    this.lastClaimTimestamp = !!claim ? new BigNumber((claim.data as any).last_claim_timestamp) : ZERO
+  }
+}
