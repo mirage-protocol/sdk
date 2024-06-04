751f783e4d51ae437a97a8180d9b426924866903
MoonShiesty <moonshiesty@protonmail.com>
2024-06-04 02:26:15 -0500

    accountviews

--- src/views/accountViews.ts
+++ src/views/accountViews.ts
@@ -1,21 +1,31 @@
 import { AccountAddress, Network } from '@aptos-labs/ts-sdk'
-
-import { aptosClient, assetBalanceToDecimal, assetInfo, getAssetTokenMetadata, getTypeFromMoveAsset, MoveAsset, ZERO } from '../constants'
 import BigNumber from 'bignumber.js'
+
+import {
+  aptosClient,
+  assetBalanceToDecimal,
+  assetInfo,
+  getAssetTokenMetadata,
+  getTypeFromMoveAsset,
+  MoveAsset,
+  ZERO,
+} from '../constants'
 
 export const getUserAssetBalance = async (
   userAddress: AccountAddress,
   asset: MoveAsset,
   network: Network
-): Promise<number> => {
+): Promise<BigNumber> => {
   let balance = ZERO
   switch (getTypeFromMoveAsset(asset)) {
     case 'MoveCoin':
-      balance = BigNumber(await aptosClient(network).getAccountCoinAmount({
-        accountAddress: userAddress.toStringLong(),
-        coinType: assetInfo(asset).type as `${string}::${string}::${string}`,
-      }))
-      break;
+      balance = BigNumber(
+        await aptosClient(network).getAccountCoinAmount({
+          accountAddress: userAddress.toStringLong(),
+          coinType: assetInfo(asset).type as `${string}::${string}::${string}`,
+        })
+      )
+      break
     case 'MoveToken':
       const data = await aptosClient(network).getCurrentFungibleAssetBalances({
         options: {
@@ -27,7 +37,7 @@ export const getUserAssetBalance = async (
         },
       })
       balance = BigNumber(data[0]?.amount ?? 0)
-      break;
+      break
   }
-  return assetBalanceToDecimal(balance, asset).toNumber()
+  return assetBalanceToDecimal(balance, asset)
 }
